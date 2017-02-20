dd4ee08cb39f5d14f87b59586e6d385dc9d0b6cc
notEave <steamhoki@gmail.com>
2017-02-20 11:10:40 +0200

    repo init, InputHandler script

---
+++ Roguelike/Assets/InputHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler {
+
+  public static float StrafeAxis() { return Input.GetAxis("Horizontal"); }
+  public static float WalkAxis  () { return Input.GetAxis("Vertical"  ); }
+
+  public static float MouseX() { return Input.GetAxis("Mouse X"); }
+  public static float MouseY() { return Input.GetAxis("Mouse Y"); }
+
+  public static bool Hit      () { return Input.GetButtonDown("Hit"      ); }
+  public static bool Block    () { return Input.GetButtonDown("Block"    ); }
+  public static bool Magic    () { return Input.GetButtonDown("Magic"    ); }
+  public static bool Activate () { return Input.GetButtonDown("Activate" ); }
+  public static bool Inventory() { return Input.GetButtonDown("Inventory"); }
+}
