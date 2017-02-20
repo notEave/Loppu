b1581f08274841f4faed8f32c475ff84139e78aa
notEave <steamhoki@gmail.com>
2017-02-20 14:30:08 +0200

    repository sync

---
+++ Roguelike/Assets/PlayerMouseLook.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+
+public class PlayerMouseLook : MonoBehaviour {
+  private Transform goTransform    ;
+  private Transform cameraTransform;
+  private Vector2   rotation       ;
+
+  public float clampCeil        = -85f;
+  public float clampFloor       =  85f;
+  public float mouseSensitivity =   3f;
+
+  private void Awake() {
+    goTransform     = transform                    .GetComponent<Transform>();
+    cameraTransform = transform.FindChild("Camera").GetComponent<Transform>();
+  }
+
+  private void Start() {
+
+  }
+}
