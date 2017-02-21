6401135ac18553e50c160b89ffbbdc10962fa57f
notEave <steamhoki@gmail.com>
2017-02-21 13:52:03 +0200

    room generation

---
+++ Roguelike/Assets/RoomBuilder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RoomBuilder : MonoBehaviour {
+  public int width  = 20;
+  public int height = 10;
+
+  private GameObject[] a;
+
+  private void Awake() {
+    GameObject.CreatePrimitive(PrimitiveType.Quad);
+  }
+
+  private void Start() {
+    a = new GameObject[width * height];
+
+  }
+}
